a38a2b321794d6dcb5a95e401aa09c87c3151f33
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-08-07 13:29:36 -0400

    Fix SEO image

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,10 @@ module.exports = {
       summary: `He is an experienced software developer with eight years of technical experience.`,
     },
     description: `Technical blog featuring writings on developing and operating software.`,
+<<<<<<< Updated upstream
+=======
+    image: `/seo-image.png`,
+>>>>>>> Stashed changes
     siteUrl: `https://technoblather.ca`,
     social: {
       email: "laakso.mavrick@gmail.com",
