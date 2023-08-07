fcdfc2a31b11523715a923ae4d5b2329da1309b0
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-08-07 12:25:36 -0400

    First draft of lambda cold start blog post

--- terraform/modules/blog/iam.tf
+++ terraform/modules/blog/iam.tf
@@ -5,6 +5,7 @@ resource "aws_iam_openid_connect_provider" "github_provider" {
   client_id_list = ["sts.amazonaws.com"]
 
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+
 }
 
 // TODO: come back to this at a later date - scope creep with this change
